1bcd4a62670732f8da936f8b019d407ca508778a
vKochanov78 <vale78.1998@gmail.com>
2024-07-01 10:02:28 +0300

    Uploading

---
+++ JS Syntax Fundamentals/Lab/12. reverseAnArrayOfNumbers.js
@@ -0,0 +1,14 @@
+function solve(n, input) {
+    let arr=[];
+    let output = '';
+
+    for (let i=o; i<n; i++){
+        arr.push(i);
+    }
+
+    for(let i=arr.length-1; i>=0; i--){
+        console.log(arr);
+    }
+}
+
+solve();
